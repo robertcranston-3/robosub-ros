d85c21f78284274691e6883aba63f42ac90f2040
Jake Heller <jake.heller@duke.edu>
2020-10-17 14:43:57 -0400

    add get velocity functions

--- landside/catkin_ws/src/simulation/scripts/PhysicsLibrary.c
+++ landside/catkin_ws/src/simulation/scripts/PhysicsLibrary.c
@@ -1,7 +1,29 @@
-int arr[3];
+float* getLinDrag(float dragCoef, float* linVel, int linVelSize, float diameter, float height){
+	float dragForce[3];
+	for (int i = 0; i < linVelSize; i++){
+		dragForce[i] = 0.5 * 1000 * dragCoef * area * linVel[i] * linVel[i];
 
-int* arr1
+	}
+}
 
-float* getLinearDrag(float dragCoef, float* linVel, float diameter, float height){
-	float dragForce = 0.5 * 1000 * dragCoef * area * 
+void applyLinDrag(float* force, int objectHandle){
+
+}
+
+float* getLinVelocity(int objectHandle){
+	int velSize = 3;
+	float* linVel = (float*) malloc(sizeof(float) * velSize);
+	float* angVel = (float*) malloc(sizeof(float) * velSize);
+	int errorCode = simGetObjectVelocity(objectHandle, linVel, angVel);
+	free(angVel);
+	return linVel;
+}
+
+float* getAngVelocity(int objectHandle){
+	int velSize = 3;
+	float* linVel = (float*) malloc(sizeof(float) * velSize);
+	float* angVel = (float*) malloc(sizeof(float) * velSize);
+	int errorCode = simGetObjectVelocity(objectHandle, linVel, angVel);
+	free(linVel);
+	return angVel;
 }
